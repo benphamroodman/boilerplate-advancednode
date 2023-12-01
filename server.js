cc1a966546d77b24806d1bd8b51670969768864c
benphamroodman <bnroodman@gmail.com>
2023-12-01 10:55:11 -0500

    Routes!

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
 const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -20,21 +21,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
-  saveUnitialized: true,
+  saveUninitialized: true,
   cookie: {secure: false}
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+/*app.route('/').get((req, res) => {
+  res.render('index', {title: 'Hello', message: 'Please log in', showLogin: true}); 
+});*/
+
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
-  app.route('/').get((req, res) => {
-    res.render('index', {
-      title: 'Connected to Databse',
-      message: 'Please login'
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({username: username}, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (password != user.password) return done(null, false);
+      return done(null, user);  
     });
-  });
+  }));
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
@@ -46,19 +55,41 @@ myDB(async client => {
     })
   });
 
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Connected to Database', 
+      message: 'Please log in',
+      showLogin: true
+    });
+  });
+
+  app.route('/login').post(passport.authenticate('local', {failureRedirect: '/'}), (req, res) => {
+    res.redirect('/profile');
+  });
+  
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    res.render('profile', {username: req.user.username});
+  });
+
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('index', {title: e, message: 'Unable to connect to database'});
   });
 });
 
-
-
-app.route('/').get((req, res) => {
-  res.render('index', {title: 'Hello', message: 'Please log in'}); 
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/');
+}
